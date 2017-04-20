df0dd56cb363a545df6de2186908f107bb057496
qiqiW <limengqi057@gmail.com>
2017-04-20 17:03:21 +0800

    zookeeper

--- PY/test1/learning/kafka_s/zookeeper_s/1_test.py
+++ PY/test1/learning/kafka_s/zookeeper_s/1_test.py
@@ -1,10 +1,10 @@
-from kazoo.client import KazooClient,KazooState
+from kazoo.client import KazooClient,KazooState,KeeperState
 import logging
 import sys
 
 logging.basicConfig()
 
-zk = KazooClient(hosts='localhost:2181')
+zk = KazooClient(hosts='192.168.202.129:2181')
 zk.start()
 
 # zk.ensure_path("/my/favorite")
@@ -21,10 +21,32 @@ zk.start()
 
 # children = zk.get_children("/my/favorite", watch=my_func)
 # print(children)
+#zk.create("/my/favorite/node","how are you ?".encode())
 @zk.ChildrenWatch("/my/favorite")
 def watch_children(children):
     print("Children are now: %s" % children)
-@zk.DataWatch("/my/favorite/node1")
+@zk.DataWatch("/my/favorite/node")
 def watch_node(data, stat):
-    print("Version: %s, data: %s" % (stat.version, data.decode("utf-8")))
+    print("Version: %s, data: %s" % (stat, data.decode("utf-8")))
+
+def my_listener(state):
+    if state == KazooState.LOST:
+        print('lost')
+        # Register somewhere that the session was lost
+    elif state == KazooState.SUSPENDED:
+        # Handle being disconnected from Zookeeper
+        print('cccc')
+    else:
+        # Handle being connected/reconnected to Zookeeper
+        print('ee')
+
+zk.add_listener(my_listener)
+# @zk.add_listener
+# def watch_for_ro(state):
+#     print(state)
+#     if state == KazooState.CONNECTED:
+#         if zk.client_state == KeeperState.CONNECTED_RO:
+#             print("Read only mode!")
+#         else:
+#             print("Read/Write mode!")
 zk.stop()
