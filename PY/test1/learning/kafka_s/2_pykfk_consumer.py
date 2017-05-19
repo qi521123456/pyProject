b1e8aa06737c2436cea4feb79c012de4adb1ae5e
qiqiW <limengqi057@gmail.com>
2017-05-19 17:56:32 +0800

    lr

--- PY/test1/learning/kafka_s/2_pykfk_consumer.py
+++ PY/test1/learning/kafka_s/2_pykfk_consumer.py
@@ -1,10 +1,13 @@
 from pykafka import KafkaClient
-client = KafkaClient()
+from pykafka.common import OffsetType
+client = KafkaClient(hosts="45.76.24.153:9092")
 print(client.topics['test'.encode()])
 topic = client.topics[b'test']
-consumer = topic.get_simple_consumer()
+consumer = topic.get_simple_consumer(consumer_group=b'testgroup',
+                reset_offset_on_start=True,
+                auto_offset_reset=OffsetType.LATEST)
 for message in consumer:
     if message is not None:
-        print(message.offset, message.value.decode())
+        print(message.offset, eval(message.value))
     else:
         print('i am down')
