ad2cd4053c1b5b5eba4f626b88559b2357f45fb1
qiqiW <limengqi057@gmail.com>
2017-06-01 17:34:15 +0800

    nono

--- PY/test1/sol_task/vps/alterfile.py
+++ PY/test1/sol_task/vps/alterfile.py
@@ -3,12 +3,12 @@ for i in range(2,11):
     try:
         host = "vps"+str(i)
         # info = os.popen("ssh " + host + " 'ps aux|grep python3'")
+        # time.sleep(1)
+        # os.popen("ssh " + host + " 'rm /opt/sol_netscan/utils*'")
+        # time.sleep(1)
+        # os.popen("scp /root/temp/* root@%s:/opt/sol_netscan/" % host)
         time.sleep(1)
-        os.popen("ssh " + host + " 'rm /opt/sol_netscan/utils*'")
-        time.sleep(1)
-        os.popen("scp /root/temp/* root@%s:/opt/sol_netscan/" % host)
-        time.sleep(1)
-        os.popen("ssh "+ host + " 'python3 /opt/sol_netscan/net_sniffer.py'")
+        os.popen("ssh "+ host + " 'nohup python3 /opt/sol_netscan/net_sniffer.py > /dev/null 2>&1 &'")
     except:
         print(i,"--wrong--")
 
