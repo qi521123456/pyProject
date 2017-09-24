ae38ed0ebe29fdc7a571e41a645d788c906db6c4
Mannix <limengqi057@gmail.com>
2017-09-24 23:35:30 +0800

    adaboost

--- PY/test1/ml/relearn/adaboost_1/adaboost.py
+++ PY/test1/ml/relearn/adaboost_1/adaboost.py
@@ -1,12 +1,80 @@
-import numpy as np
+from numpy import *
+import sklearn
 def loadSimpData():
-    datMat = np.matrix([[1.,2.1],[2.,1.1],[1.3,1.],[1.,1.],[2.,1.]])
+    datMat = matrix([[ 1. ,  2.1],
+        [2.,  1.1],
+        [ 1.3,  1. ],
+        [ 1. ,  1. ],
+        [ 2. ,  1. ]])
+    classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
+    return datMat,classLabels
+
+
+def stumpClassify(dataMatrix, dimen, threshVal, threshIneq):  # just classify the data
+    retArray = ones((shape(dataMatrix)[0], 1))
+    if threshIneq == 'lt':
+        retArray[dataMatrix[:, dimen] <= threshVal] = -1.0
+    else:
+        retArray[dataMatrix[:, dimen] > threshVal] = -1.0
+    return retArray
+
+
+def buildStump(dataArr, classLabels, D):
+    dataMatrix = mat(dataArr)
+    labelMat = mat(classLabels).T
+    m, n = shape(dataMatrix)
+    numSteps = 10.0
+    bestStump = {}
+    bestClasEst = mat(zeros((m, 1)))
+    minError = inf  # init error sum, to +infinity
+    for i in range(n):  # loop over all dimensions
+        rangeMin = dataMatrix[:, i].min()
+        rangeMax = dataMatrix[:, i].max()
+        stepSize = (rangeMax - rangeMin) / numSteps
+        for j in range(-1, int(numSteps) + 1):  # loop over all range in current dimension
+            for inequal in ['lt', 'gt']:  # go over less than and greater than
+                threshVal = (rangeMin + float(j) * stepSize)
+                predictedVals = stumpClassify(dataMatrix, i, threshVal,
+                                              inequal)  # call stump classify with i, j, lessThan
+                errArr = mat(ones((m, 1)))
+                errArr[predictedVals == labelMat] = 0
+                weightedError = D.T * errArr  # calc total error multiplied by D
+                # print ("split: dim %d, thresh %.2f, thresh ineqal: %s, the weighted error is %.3f" % (i, threshVal, inequal, weightedError))
+                if weightedError < minError:
+                    minError = weightedError
+                    bestClasEst = predictedVals.copy()
+                    bestStump['dim'] = i
+                    bestStump['thresh'] = threshVal
+                    bestStump['ineq'] = inequal
+    return bestStump, minError, bestClasEst
+
+def adaBoostTrainDS(dataArr,classLabels,numIt=40):
+    weakClassArr = []
+    m = shape(dataArr)[0]
+    D = mat(ones((m,1))/m)   #init D to all equal
+    aggClassEst = mat(zeros((m,1)))
+    for i in range(numIt):
+        bestStump,error,classEst = buildStump(dataArr,classLabels,D)#build Stump
+        print("D:",D.T)
+        alpha = float(0.5*log((1.0-error)/max(error,1e-16)))#calc alpha, throw in max(error,eps) to account for error=0
+        bestStump['alpha'] = alpha
+        weakClassArr.append(bestStump)                  #store Stump Params in Array
+        print("classEst: ",classEst.T)
+        expon = multiply(-1*alpha*mat(classLabels).T,classEst) #exponent for D calc, getting messy
+        D = multiply(D,exp(expon))                              #Calc New D for next iteration
+        D = D/D.sum()
+        #calc training error of all classifiers, if this is 0 quit for loop early (use break)
+        aggClassEst += alpha*classEst
+        print("aggClassEst: ",aggClassEst.T)
+        aggErrors = multiply(sign(aggClassEst) != mat(classLabels).T,ones((m,1)))
+        # print(aggErrors)
+        errorRate = aggErrors.sum()/m
+        print("total error: ",errorRate)
+        if errorRate == 0.0: break
+    return weakClassArr,aggClassEst
 
 if __name__ == '__main__':
-    #print(np.shape(np.mat([[1.,2.1],[2.,1.1],[1.3,1.],[1.,1.],[2.,1.]])))
-    l = np.mat([[1,2],[3,4],[5,6]])
-    a = np.mat([[1],[1],[1]])
-    #a[<=2] = -1
-    print(l[:2, 0])
-    a[l[:, 1] <= 2] = -1
-    print(np.ones(55))
+    datMat, labels = loadSimpData()
+    print(adaBoostTrainDS(datMat,labels))
+    # print(multiply(False,11))
+    #print(arange(9.0).reshape(9,1))
