5599cfd487a28387bc1c5bff9c02ec9c8a0a38e2
qiqiW <limengqi057@gmail.com>
2017-09-28 18:08:25 +0800

    new little

--- PY/test1/sol_task/scan_relate/2_contr.py
+++ PY/test1/sol_task/scan_relate/2_contr.py
@@ -1,7 +1,7 @@
 import os,re
 
-protocolList = []
-portList = []
+protocolList = ["rtsp","S7Comm","EtherNetIP","Modbus"]
+portList = ["80","21"]
 tests = ["test1","test2","test3"]
 path = "/root/data/"
 def main():
@@ -11,10 +11,10 @@ def main():
             if re.match('^1.*txt$', filename) is None:
                 os.system("rm -rf %s"%filedir+filename)
         for protocol in protocolList:
-            cmd = "nohup docker run -v /root/data:/data tmpnmap python /data/newscan.py --test=" + test + " --country=h --node=1 --protocol="+protocol
+            cmd = "nohup docker run -v /root/data:/data tmpnmap python /data/newscan.py --test=" + test + " --country=h --node=1 --protocol="+protocol + "> /dev/null 2>&1 &"
             os.popen(cmd)
         for port in portList:
-            cmd = "nohup docker run -v /root/data:/data tmpnmap python /data/newscan.py --test=" + test + " --country=h --node=1 --port=" + port
+            cmd = "nohup docker run -v /root/data:/data tmpnmap python /data/newscan.py --test=" + test + " --country=h --node=1 --port=" + port + "> /dev/null 2>&1 &"
             os.popen(cmd)
 
 
